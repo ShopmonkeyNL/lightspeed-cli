48a9de5f5f287b079f99eb74ac907fc908960e96
Robin de Laater <robin.delaater@shopmonkey.nl>
2024-04-25 10:40:27 +0200

    Update gulp and packages (#20)

--- src/WatcherFiles/gulpfile.js
+++ src/WatcherFiles/gulpfile.js
@@ -1,53 +1,33 @@
-const config = require('./gulp.config');
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const del = require('del');
-const gulp = require('gulp');
-const watch = require('gulp-watch');
-const rename = require('gulp-rename');
-const sass = require('gulp-sass')(require('sass'));
-const uglifycss = require('gulp-uglifycss');
+const { watch, src, task } = require('gulp');
 const exec = require('gulp-exec');
 
-const watcher = watch(['theme/layouts/*.rain', 'theme/pages/*.rain', 'theme/snippets/*.rain', 'theme/assets/*', 'src/**/*']);
-
-gulp.task('watch', function () {
+const watcher = watch([['theme/layouts/*.rain', 'theme/pages/*.rain', 'theme/snippets/*.rain', 'theme/assets/*', 'src/**/*']]);
+task('watch', () => {
     let path = '';
-    watcher.on('change', function (path, stats) {
-        if (path.endsWith('.scss')) {
-            gulp.parallel('sass')(path);
-        }
-
+    watcher.on('change', (path, stats) => {
         if (!path.includes('src')) {
-            return gulp.src('/')
-                .pipe(exec('php ".functions/filewatcher.php" ' + '"' + path + '" change'))
-                .pipe(exec.reporter());
+            return src('/')
+                .pipe(exec(`php ".functions/filewatcher.php" "${path}" change`))
+                .pipe(exec.reporter())
+                .on('end', () => this.emit('end'));
         }
     });
-    watcher.on('add', function (path, stats) {
+
+    watcher.on('add', (path, stats) => {
         if (!path.includes('src')) {
-            return gulp.src('/')
-                .pipe(exec('php ".functions/filewatcher.php" ' + '"' + path + '" add'))
-                .pipe(exec.reporter());
+            return src('/')
+                .pipe(exec(`php ".functions/filewatcher.php" "${path}" add`))
+                .pipe(exec.reporter())
+                .on('end', () => this.emit('end'));
         }
     });
-    watcher.on('unlink', function (path, stats) {
+
+    watcher.on('unlink', (path, stats) => {
         if (!path.includes('src')) {
-            return gulp.src('/')
-                .pipe(exec('php ".functions/filewatcher.php" ' + '"' + path + '" unlink'))
-                .pipe(exec.reporter());
+            return src('/')
+                .pipe(exec(`php ".functions/filewatcher.php" "${path}" unlink`))
+                .pipe(exec.reporter())
+                .on('end', () => this.emit('end'));
         }
     });
-});
-
-gulp.task('sass', () => {
-    del(config.css.sourcePaths + '**/*', { force: true });
-
-    return gulp.src(config.css.sourcePaths)
-        .pipe(sass(config.thirdParty.sassOptions).on('error', sass.logError))
-        .pipe(concat('bundle.css'))
-        .pipe(autoprefixer())
-        .pipe(uglifycss(config.thirdParty.uglifyCssOptions))
-        // .pipe(rename({ suffix: '.min' })) current setup does not allow double file extensions
-        .pipe(gulp.dest(config.css.exportPath))
 });
