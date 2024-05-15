54713f39cdf15f542a60799693ff69f287d3ba3a
Robin de Laater <robin.delaater@shopmonkey.nl>
2024-05-15 14:20:36 +0200

    Update globs

--- src/WatcherFiles/gulpfile.js
+++ src/WatcherFiles/gulpfile.js
@@ -1,33 +1,39 @@
-const { watch, src, task } = require('gulp');
-const exec = require('gulp-exec');
+const { watch, src, task } = require("gulp");
+const exec = require("gulp-exec");
 
-const watcher = watch([['theme/layouts/*.rain', 'theme/pages/*.rain', 'theme/snippets/*.rain', 'theme/assets/*', 'src/**/*']]);
-task('watch', () => {
-    let path = '';
-    watcher.on('change', (path, stats) => {
-        if (!path.includes('src')) {
-            return src('/')
-                .pipe(exec(`php ".functions/filewatcher.php" "${path}" change`))
-                .pipe(exec.reporter())
-                .on('end', () => this.emit('end'));
-        }
-    });
+const watcher = watch([
+  "theme/layouts/*.rain",
+  "theme/pages/*.rain",
+  "theme/snippets/*.rain",
+  "theme/assets/*",
+  "src/**/*",
+]);
+task("watch", () => {
+  let path = "";
+  watcher.on("change", (path, stats) => {
+    if (!path.includes("src")) {
+      return src("/")
+        .pipe(exec(`php ".functions/filewatcher.php" "${path}" change`))
+        .pipe(exec.reporter())
+        .on("end", () => this.emit("end"));
+    }
+  });
 
-    watcher.on('add', (path, stats) => {
-        if (!path.includes('src')) {
-            return src('/')
-                .pipe(exec(`php ".functions/filewatcher.php" "${path}" add`))
-                .pipe(exec.reporter())
-                .on('end', () => this.emit('end'));
-        }
-    });
+  watcher.on("add", (path, stats) => {
+    if (!path.includes("src")) {
+      return src("/")
+        .pipe(exec(`php ".functions/filewatcher.php" "${path}" add`))
+        .pipe(exec.reporter())
+        .on("end", () => this.emit("end"));
+    }
+  });
 
-    watcher.on('unlink', (path, stats) => {
-        if (!path.includes('src')) {
-            return src('/')
-                .pipe(exec(`php ".functions/filewatcher.php" "${path}" unlink`))
-                .pipe(exec.reporter())
-                .on('end', () => this.emit('end'));
-        }
-    });
+  watcher.on("unlink", (path, stats) => {
+    if (!path.includes("src")) {
+      return src("/")
+        .pipe(exec(`php ".functions/filewatcher.php" "${path}" unlink`))
+        .pipe(exec.reporter())
+        .on("end", () => this.emit("end"));
+    }
+  });
 });
